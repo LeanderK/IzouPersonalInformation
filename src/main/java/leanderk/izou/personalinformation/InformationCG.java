33e2c3cb966a16bcd82ecc576a2ed55d42e1657f
Leander Kurscheidt <Leander.Kurscheidt@gmx.de>
2014-10-30 09:37:07 +0100

    changed version number

--- src/main/java/leanderk/izou/personalinformation/InformationCG.java
+++ src/main/java/leanderk/izou/personalinformation/InformationCG.java
@@ -24,8 +24,41 @@ public class InformationCG extends ContentGenerator<HashMap <String, String>>{
         this.properties = properties;
     }
 
+    /**
+     * returns ContentData with the following format.
+     *
+     * ContentData is a HashMap,where the keys and values are strings.
+     * ALWAYS check for existence of key/value pairs
+     *
+     * #basic data:
+     *
+     * #first name of the user:
+     * firstname
+     * #last name of the user:
+     * lastname
+     *
+     *
+     * #email
+     * email
+     *
+     *
+     * #location data
+     *
+     * #postal code
+     * postalcode
+     * #town
+     * town
+     * #street
+     * street
+     * #housenumer
+     * housenumber
+     *
+     * @param id id the event causing the generate
+     * @return ContentData containing information about the user
+     * @throws Exception
+     */
     @Override
-    public ContentData<HashMap <String, String>> generate(String s) throws Exception {
+    public ContentData<HashMap <String, String>> generate(String id) throws Exception {
         HashMap<String, String> map = new HashMap<>();
         for (final String name: properties.stringPropertyNames())
             map.put(name, properties.getProperty(name));
